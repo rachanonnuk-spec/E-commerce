c701bb23518942ec9534652b76b5e37419c82add
Rachanon Srichai <rachanon.nuk@gmail.com>
2026-05-23 16:43:13 +0700

    added cart count func

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import {assets} from '../assets/assets'
 function Footer() {
   return (
     <div>
-        <div className='flex flex-col sm:grind grid-col-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
 
             <div>
                 <img src={assets.logo} className='mb-5 w-32' alt="" />

c9f3e913be1b14ed39237f23f0584e8b358f16cc
Rachanon Srichai <rachanon.nuk@gmail.com>
2026-05-23 18:05:37 +0700

    added cart add delete item function

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -59,10 +59,18 @@ const ShopContextProvider = (props) => {
     //     console.log(cartItems);
     // },[cartItems]);
 
+
+    const updateQuantity = async (itemId,size,quantity) => {
+        let cartData = structuredClone(cartItems);
+        cartData[itemId][size] = quantity
+
+        setCartItems(cartData);
+    }
+
     const value = {
         products,currency,delivery_fee,
         search,setSearch,showSearch,setShowSearch,
-        cartItems,addToCart,getCartCount
+        cartItems,addToCart,getCartCount,updateQuantity
     }
 
     return (
